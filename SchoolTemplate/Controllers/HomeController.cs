e98dcd167ebc15305a26487642c621fba320f5e4
110074@st-maartenscollege.nl <110074@st-maartenscollege.nl>
2020-01-17 14:50:00 +0100

    style changes

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace SchoolTemplate.Controllers
     // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
     string connectionString = "Server=172.16.160.21;Port=3306;Database=110157;Uid=110157;Pwd=crOLeran;";
 
+    [Route("Home/Index")]
     public IActionResult Index()
     {
       List<Product> products = new List<Product>();
